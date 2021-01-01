e80c4b565747d4a0ff663763de65e4602d14fe25
donghunee <ehdgns1766@naver.com>
2021-01-02 04:08:14 +0900

    [skip ci]first

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React Test Deploy
+          React Test Deploy_cancle
         </a>
       </header>
     </div>
