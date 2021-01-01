d5fec8127375c7c672c22e6d2766ebdb16ba6825
donghunee <ehdgns1766@naver.com>
2021-01-02 04:32:56 +0900

    fisrt

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React Test Deploy_cancle
+          React Test Deploy_ cloud_front
         </a>
       </header>
     </div>
